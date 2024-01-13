7e79c18059193f46994a5924c1c7c0e08b9159e9
Eduardo <cesconettoedu@gmail.com>
2024-01-13 11:20:48 -0500

    create a form to enter user number

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [userNum, setUserNum] = useState(1);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  };
+
   return (
     <div className="App">
-      
+      <div className="col-8 m-4">
+        <h2>User Posts List</h2>
+        
+        <form onSubmit={handleSubmit}>
+          <label >Enter user number :  </label>
+          <input
+            type="text"
+            value={userNum}
+            onChange={(e) => setUserNum(e.target.value)}
+          />
+          <input type="submit" />
+        </form>
+      </div>
     </div>
   );
 }
